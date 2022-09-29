adc56e72686fb97e636474772a99986c5ad76895
Rauulito <7rnavarro7@gmail.com>
2022-09-29 23:00:03 +0200

    cambiado

--- calcu/calculadora-tdd-01-empezamos/tests/test_calculator.py
+++ calcu/calculadora-tdd-01-empezamos/tests/test_calculator.py
@@ -12,7 +12,15 @@ from calculator import Calculator
 # Creamos una clase heredando de TestCase
 class TestMyCalculator(unittest.TestCase):
 # Creamos una prueba para probar un valor inicial
+    def setUp(self):
+        self.calc = Calculator()
     def test_initial_value(self):
-        calc = Calculator()
-        self.assertEqual(0, calc.value)
+        self.assertEqual(0, self.calc.value)
+    # Creamos un nuevo test para comprobar una suma
+    def test_add_method(self):
+        # Ejecutamos el método
+        self.calc.add(1, 3)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(4, self.calc.value)
+
 
